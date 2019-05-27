3d022fd978a05c1fb034fe6688cb85e3b840284b
Seungjae Park <astro.psj@gmail.com>
2019-05-27 22:14:43 +0900

    Implement Debug, Extend, and FromIterator traits

--- src/collections.rs
+++ src/collections.rs
@@ -1,9 +1,45 @@
 use std::collections::HashMap;
+use std::fmt::{Debug, Formatter, Result};
+use std::iter::FromIterator;
 
 pub struct Dictionary<'a> {
     indexes: std::collections::HashMap<&'a str, u32>,
     size: u32,
 }
+
+impl<'a> Debug for Dictionary<'a> {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        write!(f, "Dictionary {:#?}", self.indexes)
+    }
+}
+
+impl<'a> Extend<&'a str> for Dictionary<'a> {
+    fn extend<I: IntoIterator<Item = &'a str>>(&mut self, tokens: I) {
+        let mut max = self.size;
+        for token in tokens {
+            if !self.indexes.contains_key(&token) {
+                max += 1;
+                self.indexes.insert(token, max);
+            }
+        }
+        self.size = max;
+    }
+}
+
+impl<'a> FromIterator<&'a str> for Dictionary<'a> {
+    fn from_iter<I: IntoIterator<Item = &'a str>>(tokens: I) -> Dictionary<'a> {
+        let mut indexes = HashMap::new();
+        let mut max = 0;
+        for token in tokens {
+            if !indexes.contains_key(&token) {
+                max += 1;
+                indexes.insert(token, max);
+            }
+        }
+        Dictionary { indexes, size: max }
+    }
+}
+
 
 impl<'a> Dictionary<'a> {
     pub fn new() -> Dictionary<'a> {
@@ -11,29 +47,6 @@ impl<'a> Dictionary<'a> {
             indexes: HashMap::new(),
             size: 0,
         }
-    }
-
-    pub fn from(tokens: &'a Vec<&'a str>) -> Dictionary {
-        let mut indexes = HashMap::new();
-        let mut max = 0;
-        for token in tokens {
-            if !indexes.contains_key(token) {
-                max += 1;
-                indexes.insert(*token, max);
-            }
-        }
-        Dictionary { indexes, size: max }
-    }
-
-    pub fn extend(&mut self, tokens: &'a Vec<&'a str>) {
-        let mut max = self.size;
-        for token in tokens {
-            if !self.indexes.contains_key(token) {
-                max += 1;
-                self.indexes.insert(*token, max);
-            }
-        }
-        self.size = max;
     }
 
     pub fn get(&self, token: &'a str) -> u32 {
@@ -52,20 +65,36 @@ pub struct FrequencyDistribution<'a> {
     frequencies: std::collections::HashMap<&'a str, u32>,
 }
 
-impl<'a> FrequencyDistribution<'a> {
-    pub fn new(tokens: &'a Vec<&'a str>) -> FrequencyDistribution {
+impl<'a> Debug for FrequencyDistribution<'a> {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        write!(f, "FrequencyDistribution {:#?}", self.frequencies)
+    }
+}
+
+impl<'a> Extend<&'a str> for FrequencyDistribution<'a> {
+    fn extend<I: IntoIterator<Item = &'a str>>(&mut self, tokens: I) {
+        for token in tokens {
+            let frequency = self.frequencies.get(&token).unwrap_or(&0) + 1;
+            self.frequencies.insert(token, frequency);
+        }
+    }
+}
+
+impl<'a> FromIterator<&'a str> for FrequencyDistribution<'a> {
+    fn from_iter<I: IntoIterator<Item = &'a str>>(tokens: I) -> FrequencyDistribution<'a> {
         let mut frequencies = HashMap::new();
         for token in tokens {
-            let frequency = frequencies.get(token).unwrap_or(&0) + 1;
-            frequencies.insert(*token, frequency);
+            let frequency = frequencies.get(&token).unwrap_or(&0) + 1;
+            frequencies.insert(token, frequency);
         }
         FrequencyDistribution { frequencies }
     }
+}
 
-    pub fn extend(&mut self, tokens: &'a Vec<&'a str>) {
-        for token in tokens {
-            let frequency = self.frequencies.get(token).unwrap_or(&0) + 1;
-            self.frequencies.insert(*token, frequency);
+impl<'a> FrequencyDistribution<'a> {
+    pub fn new() -> FrequencyDistribution<'a> {
+        FrequencyDistribution {
+            frequencies: HashMap::new(),
         }
     }
 
--- src/preprocessing.rs
+++ src/preprocessing.rs
@@ -1,11 +1,12 @@
 use super::collections::Dictionary;
+use std::iter::FromIterator;
 
-pub fn encode_int(tokens: &Vec<&str>) -> Vec<u32> {
-    let indexes = Dictionary::from(tokens);
+pub fn encode_int(tokens: Vec<&str>) -> Vec<u32> {
+    let indexes = Dictionary::from_iter(tokens.clone().into_iter());
     tokens.iter().map(|token| indexes.get(*token)).collect()
 }
 
-pub fn encode_one_hot(tokens: &Vec<&str>) -> Vec<Vec<u32>> {
+pub fn encode_one_hot(tokens: Vec<&str>) -> Vec<Vec<u32>> {
     let ints = encode_int(tokens);
     let max = *ints.iter().max().unwrap();
     let len = max as usize + 1;
